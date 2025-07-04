9172d362ba03cd35ea1f97130de336ada7052e7d
stukebe <stuckeyben@gmail.com>
2025-07-04 12:53:01 -0500

    G: added group containers

--- main.py
+++ main.py
@@ -12,15 +12,26 @@ def main():
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	#create groups
+	updatables = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updatables, drawables)
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+	
+
+
+	#game loop starts
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill(000000)
-		player.update(dt)
-		player.draw(screen)
+		updatables.update(dt)
+		for drawable in drawables:
+			drawable.draw(screen)
 		pygame.display.flip()
 		clock.tick(60)
 		dt = clock.tick(60) / 1000
