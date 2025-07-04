98de0c489fdd7146518da2bb9a762f346b34c8c1
stukebe <stuckeyben@gmail.com>
2025-07-04 12:10:31 -0500

    D: player now prints on screen

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
@@ -11,11 +13,13 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill(000000)
+		player.draw(screen)
 		pygame.display.flip()
 		clock.tick(60)
 		dt = clock.tick(60) / 1000
