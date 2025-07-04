4a3585b719181cffcfdd4c400834c9f18e15653b
stukebe <stuckeyben@gmail.com>
2025-07-04 11:46:15 -0500

    B: clock tick set up

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 from constants import *
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +17,8 @@ def main():
         			return
 		screen.fill(000000)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
